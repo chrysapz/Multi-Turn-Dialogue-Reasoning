359095e03cd229daccbdfe319082dc89cb135f57
Chrysoula Pozrikidou <xrysa00@hotmail.com>
2023-10-07 13:56:25 +0200

    plotting data maps

--- train.py
+++ train.py
@@ -16,6 +16,9 @@ from time import gmtime, strftime
 from tqdm import tqdm
 from collections import defaultdict
 from utils import calculate_true_label_probs, count_true_label_correct, calculate_mean, calculate_variability
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def train(model, train_loader, dev_loader, optimizer, config, device):
     """
@@ -106,9 +109,95 @@ def train(model, train_loader, dev_loader, optimizer, config, device):
     variability = calculate_variability(true_label_dict_probs, confidence)
     correctness = calculate_mean(count_true_pred_dict)
 
+    #create dataframe for data_plots
+    df = pd.DataFrame(list(correctness.items()), columns=['sentence_id', 'correctness'])
+    df['confidence'] = df['sentence_id'].map(confidence)
+    df['variability'] = df['sentence_id'].map(variability)
+
     best_model_info = {'model_state_dict': best_model.state_dict(), 'epoch':best_epoch,'optimizer_state_dict':best_optimizer.state_dict()}
 
     return best_model_info, epoch_loss, confidence, variability, correctness
+
+def plot_maps(dataframe, hue_metric ='correct.', title='', model='RoBERTa', show_hist=False):
+    # Comment out if sub sampling is needed
+    #dataframe = dataframe.sample(n=1000 if dataframe.shape[0] > 25000 else len(dataframe))
+    
+    # Normalize correctness to a value between 0 and 1.
+    dataframe = dataframe.assign(corr_frac = lambda d: d.correctness / d.correctness.max())
+    dataframe['correct.'] = [f"{x:.1f}" for x in dataframe['corr_frac']]
+    
+    main_metric = 'variability'
+    other_metric = 'confidence'
+    
+    hue = hue_metric
+    num_hues = len(dataframe[hue].unique().tolist())
+    style = hue_metric if num_hues < 8 else None
+
+    if not show_hist:
+        fig, axs = plt.subplots(1, 1, figsize=(8, 4))
+        ax0 = axs
+    else:
+        fig = plt.figure(figsize=(16, 10), )
+        gs = fig.add_gridspec(2, 3, height_ratios=[5, 1])
+    
+        ax0 = fig.add_subplot(gs[0, :])
+    
+    
+    ### Make the scatterplot.
+    
+    # Choose a palette.
+    pal = sns.diverging_palette(260, 15, n=num_hues, sep=10, center="dark")
+
+    plot = sns.scatterplot(x=main_metric,
+                           y=other_metric,
+                           ax=ax0,
+                           data=dataframe,
+                           hue=hue,
+                           palette=pal,
+                           style=style,
+                           s=30)
+    
+    # Annotate Regions.
+    bb = lambda c: dict(boxstyle="round,pad=0.3", ec=c, lw=2, fc="white")
+    an1 = ax0.annotate("ambiguous", xy=(0.9, 0.5), xycoords="axes fraction", fontsize=15, color='black',
+                  va="center", ha="center", rotation=350, bbox=bb('black'))
+    an2 = ax0.annotate("easy-to-learn", xy=(0.27, 0.85), xycoords="axes fraction", fontsize=15, color='black',
+                  va="center", ha="center", bbox=bb('r'))
+    an3 = ax0.annotate("hard-to-learn", xy=(0.35, 0.25), xycoords="axes fraction", fontsize=15, color='black',
+                  va="center", ha="center", bbox=bb('b'))
+    
+    if not show_hist:
+        plot.legend(ncol=1, bbox_to_anchor=(1.01, 0.5), loc='center left', fancybox=True, shadow=True)
+    else:
+        plot.legend(fancybox=True, shadow=True,  ncol=1)
+    plot.set_xlabel('variability')
+    plot.set_ylabel('confidence')
+    
+    if show_hist:
+        plot.set_title(f"{model}-{title} Data Map", fontsize=17)
+        
+        # Make the histograms.
+        ax1 = fig.add_subplot(gs[1, 0])
+        ax2 = fig.add_subplot(gs[1, 1])
+        ax3 = fig.add_subplot(gs[1, 2])
+
+        plott0 = dataframe.hist(column=['confidence'], ax=ax1, color='#622a87')
+        plott0[0].set_title('')
+        plott0[0].set_xlabel('confidence')
+        plott0[0].set_ylabel('density')
+
+        plott1 = dataframe.hist(column=['variability'], ax=ax2, color='teal')
+        plott1[0].set_title('')
+        plott1[0].set_xlabel('variability')
+
+        plot2 = sns.countplot(x="correct.", data=dataframe, color='#86bf91', ax=ax3)
+        ax3.xaxis.grid(True) # Show the vertical gridlines
+
+        plot2.set_title('')
+        plot2.set_xlabel('correctness')
+        plot2.set_ylabel('')
+
+    fig.tight_layout()
 
 
 def main(config):
